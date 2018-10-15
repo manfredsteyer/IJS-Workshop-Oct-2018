b7fb94e2216f10d36982d5710a360e6925f787b0
ManfredSteyer <manfred.steyer@gmx.net>
2018-10-15 23:02:10 +0200

    fin

--- src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,11 +11,19 @@ export class FlightEditComponent implements OnInit {
   id: string;
   showDetails: string;
 
-  constructor(private route: ActivatedRoute) { 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) { 
+  }
+
+  next(): void {
+    const next = parseInt(this.id)+1
+    this.router.navigate(['/flight-edit', next]);
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
+      console.debug('params', params);
       this.id = params['id'];
       this.showDetails = params['showDetails'];
     });    
